aafeb84278641cd5a09c1084cb34c22f765f6005
SamBug21 <s.bugata@student.uq.edu.au>
2024-08-15 14:18:25 +1000

    Added js
    added js

---
+++ practice/js/script.js
@@ -0,0 +1,13 @@
+//Import functionality from other modules 
+
+//Define variables
+
+//Attach event listeners to elements
+
+//Startup code that runs at or during page load
+alert("JavaScript works!")
+console.log("script loaded")
+
+//Functions to define specific behaviours
+
+//Functions for general use
